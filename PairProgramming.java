c8e99f599aa28fcb26f110b5a688121571e37652
Alexandra Perez <116985888+ap26131@users.noreply.github.com>
2023-10-07 16:36:06 -0400

    Task 1
    Open Eclipse IDE, create a Java Project named tda and create a class named PairProgramming. Implement the main method header, in the main method, print "This is TDA's First Pair Programming Activity!".

--- PairProgramming.java
+++ PairProgramming.java
@@ -2,6 +2,7 @@ package tda;
 
 public class PairProgramming {
 	public static void main(String[] args) {
-	
+		// print "This is TDA's first Pair Programming Activity"
+		System.out.println("This is TDA's First Pair Programming Activity!");
 	}
 }
